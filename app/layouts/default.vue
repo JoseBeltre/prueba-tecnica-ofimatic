85df4d44d49c46be856913c4a81823595f50f2c5
Jose Beltre <552006jose@gmail.com>
2025-11-26 13:39:27 -0400

    feat: add default layout with header and main slot for content
    - imported img to use in dahsboard

---
+++ app/layouts/default.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="min-h-dvh bg-linear-to-br from-[#D0DDFF] to-[#8EADFC] p-6 pb-0">
+    <div class="bg-white min-h-dvh px-6 rounded-t-2xl">
+      <header class="flex items-center justify-between py-4">
+        <div class="flex gap-20 items-center">
+          <a class="font-black text-3xl" href="/">
+          Logo
+        </a>
+
+        <ul class="flex gap-10">
+          <li><NuxtLink to="/productos">Productos</NuxtLink></li>
+          <li><NuxtLink to="/blog">Blog</NuxtLink></li>
+        </ul>
+        </div>
+        <div class="flex items-center gap-3 font-medium">
+          <h3>Jose Beltre Cordero</h3>
+          <div class="size-10 bg-gray-400 rounded-full overflow-hidden">
+            <img src="" alt="">
+          </div>
+        </div>
+      </header>
+      <main>
+        <slot />
+      </main>
+    </div>
+  </div>
+</template>
