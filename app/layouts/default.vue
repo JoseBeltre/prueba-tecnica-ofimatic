65c5075884f96f27ef2917bcc9f31ef9f4e752bd
Jose Beltre <552006jose@gmail.com>
2025-11-27 00:39:53 -0400

    feat: enhance layout with responsive navigation and user cards on index/dashboard page

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -1,28 +1,36 @@
 <script setup>
+  import { IconMenu, IconX } from '@tabler/icons-vue';
+
   const user = useUserStore().user
+  const showMenu = ref(false)
 </script>
 <template>
-  <div class="min-h-dvh bg-linear-to-br from-[#D0DDFF] to-[#8EADFC] p-6 pb-0">
-    <div class="bg-white min-h-dvh px-6 rounded-t-2xl">
-      <header class="flex items-center justify-between py-4">
-        <div class="flex gap-20 items-center">
-          <a class="font-black text-3xl" href="/">
+  <div class="min-h-dvh bg-linear-to-br from-[#D0DDFF] to-[#8EADFC] sm:p-6 sm:pb-0 pb-0">
+    <div class="bg-white max-w-[1360px] m-auto min-h-dvh sm:px-6 sm:rounded-t-2xl">
+      <header class="max-sm:bg-[#618dff] max-sm:px-4 max-sm:text-white flex items-center justify-between py-4 max-sm:flex-row-reverse">
+        <div class="flex gap-14 md:gap-20 items-center">
+          <a class="hidden sm:block font-black text-3xl" href="/">
           Logo
         </a>
 
-        <ul class="flex gap-10">
-          <li><NuxtLink to="/productos">Productos</NuxtLink></li>
-          <li><NuxtLink to="/blog">Blog</NuxtLink></li>
+        <input id="menu-opener" v-model="showMenu" class="peer hidden" type="checkbox">
+        <ul class="max-sm:absolute hidden peer-checked:flex sm:flex max-sm:flex-col max-sm:bg-white max-sm:shadow-2xl max-sm:border-2 max-sm:border-black/20 max-sm:top-16 max-sm:right-4 max-sm:p-4 max-sm:rounded-xl :gap-2 sm:gap-10">
+          <li><NuxtLink class="hover:underline hover:text-[#3970ff] transition-colors " to="/products">Productos</NuxtLink></li>
+          <li><NuxtLink class="hover:underline hover:text-[#3970ff] transition-colors " to="/posts">Blog</NuxtLink></li>
         </ul>
+        <label class="sm:hidden" for="menu-opener">
+          <IconMenu v-if="!showMenu" />
+          <IconX v-else />
+        </label>
         </div>
-        <div class="flex items-center gap-3 font-medium text-end leading-4">
+        <div class="flex max-sm:flex-row-reverse items-center gap-3 font-medium leading-4">
           <h3>{{ user.firstName + ' ' + user.lastName }}</h3>
-          <div class="w-10 bg-gray-200 rounded-full overflow-hidden aspect-square ">
+          <div class="w-8 sm:w-10 bg-gray-200 rounded-full overflow-hidden aspect-square ">
             <img class="size-full object-cover aspect-square" :src="user.image" :alt="`Photo of ${user.username}`">
           </div>
         </div>
       </header>
-      <main>
+      <main class="max-sm:p-6">
         <slot />
       </main>
     </div>
--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,11 +1,63 @@
 <script setup>
+import { IconArrowUp, IconChevronRight } from '@tabler/icons-vue';
+import BagImg from '@/assets/imgs/dashboard/bag.png'
+import UserImg from '@/assets/imgs/dashboard/user.png'
+import PencilImg from '@/assets/imgs/dashboard/pencil.png'
+
 definePageMeta({
   middleware: ['auth']
 })
+
+const user = useUserStore().user
+
 </script>
 <template>
-  <div>
-    <h1>Index</h1>
-    <NuxtLink to="/login">Ir a Inicio de sesion</NuxtLink>
+  <div class="flex h-full flex-col">
+    <h1 class="text-4xl font-sans">Wassup, <span>{{ user.firstName }}</span></h1>
+    <nav class="hidden sm:flex mt-4">
+      <IconChevronRight />
+      <ul class="flex underline flex-wrap gap-x-5">
+        <li><NuxtLink class="cursor-pointer" to="/products">Ver productos</NuxtLink></li>
+        <li><NuxtLink class="cursor-pointer" to="/posts">Ver productos</NuxtLink></li>
+        <li><NuxtLink class="cursor-pointer" to="">Ver usuarios</NuxtLink></li>
+      </ul>
+    </nav>
+    <div class="grid grid-cols-1 min-h-full mt-4 gap-4 md:grid-rows-2 md:grid-cols-5 flex-1 h-full md:min-h-[420px]">
+      <!-- Card de Productos -->
+      <NuxtLink class="cursor-pointer flex justify-end items-end text-end relative rounded-4xl bg-linear-to-br from-[#FDEFFF] to-50% to-[#FF7CD3] p-6 h-44 overflow-hidden group md:row-span-2 md:col-span-2 md:h-auto md:items-start" to="/products">
+        <div class="size-92 md:size-120 absolute -left-36 -bottom-46 md:-left-27 md:-bottom-30 group-hover:scale-120 transition-transform duration-300">
+          <img class="size-full" :src="BagImg" alt="">
+        </div>
+        <div class="text-white z-10 mt-4 group-hover:scale-110 transition-transform duration-300">
+          <h2 class="font-extrabold text-7xl md:text-9xl drop-shadow-2xl">231</h2>
+          <h3 class="leading-2 md:leading-0 font-bold text-2xl drop-shadow-2xl">Productos</h3>
+        </div>
+        <IconArrowUp class="absolute top-4 right-4 rotate-45 text-white" size="36" stroke="1" />
+      </NuxtLink>
+      <!-- Card de Publicaiones -->
+      <NuxtLink class="cursor-pointer flex relative rounded-4xl bg-linear-to-br from-[#E08D18] to-100% to-[#FFDB78] p-6 h-44 overflow-hidden group md:h-auto md:col-span-3" to="/posts">
+        <div class="size-100 absolute -right-46 -top-46 md:-right-10 md:-top-32 rotate-20 group-hover:scale-120 transition-transform duration-300">
+          <img class="size-full" :src="PencilImg" alt="">
+        </div>
+        <div class="text-white z-10 mt-2 group-hover:scale-110 transition-transform duration-300">
+          <h2 class="font-extrabold text-7xl md:text-9xl drop-shadow-2xl">122</h2>
+          <h3 class="leading-2 md:leading-0 font-bold text-2xl drop-shadow-2xl">Publicaciones</h3>
+        </div>
+        <IconArrowUp class="absolute top-4 right-4 rotate-45 text-white" size="36" stroke="1" />
+      </NuxtLink>
+      <!-- Card de Usuarios -->
+      <NuxtLink class="cursor-pointer flex justify-end items-end text-end relative rounded-4xl bg-linear-to-br from-[#FFCECE] to-50% to-[#F97676] p-6 h-44 overflow-hidden group md:h-auto md:col-span-3 md:items-start" to="">
+        <div class="size-86 absolute -left-22 -bottom-36 md:-bottom-28 md:-left-12 group-hover:scale-120 transition-transform duration-300">
+          <img class="size-full" :src="UserImg" alt="">
+        </div>
+        <div class="text-white z-10 mt-2 me-8 group-hover:scale-110 transition-transform duration-300">
+          <h3 class="leading-4 font-bold text-2xl drop-shadow-2xl">Usuarios</h3>
+          <h2 class="font-extrabold text-7xl md:text-9xl drop-shadow-2xl">221</h2>
+          <h3 class="leading-2 md:leading-0 font-bold text-2xl drop-shadow-2xl">Activos</h3>
+        </div>
+        <IconArrowUp class="absolute top-4 right-4 rotate-45 text-white" size="36" stroke="1" />
+      </NuxtLink>
+    </div>
+    <p class="my-2 ms-2 font-medium text-black/60">Tip: Haz clic en los cuadros que tienen el icono de flecha para acceder más rápido.</p>
   </div>
 </template>
